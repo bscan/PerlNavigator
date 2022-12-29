a18cdeee13f873e04540281e105dd2e50b6a29d0
bscan <10503608+bscan@users.noreply.github.com>
2022-12-28 21:47:36 -0500

    Removes warning on eglot startup for #57

--- server/src/server.ts
+++ server/src/server.ts
@@ -90,11 +90,6 @@ connection.onInitialized(() => {
         // Register for all configuration changes.
         connection.client.register(DidChangeConfigurationNotification.type, undefined);
     }
-    if (hasWorkspaceFolderCapability) {
-        connection.workspace.onDidChangeWorkspaceFolders(_event => {
-            // connection.console.log('Workspace folder change event received.');
-        });
-    }
 });
 
 

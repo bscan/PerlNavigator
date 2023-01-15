2c17a5096fa5c9310ffea8d478e30c7175b0b125
bscan <10503608+bscan@users.noreply.github.com>
2023-01-15 17:09:29 -0500

    First version of Web Extension. Currently only offers syntax highlighting.

---
+++ client/src/browserClientMain.ts
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExtensionContext, Uri } from 'vscode';
+import { LanguageClientOptions } from 'vscode-languageclient';
+
+import { LanguageClient } from 'vscode-languageclient/browser';
+
+// this method is called when vs code is activated
+export function activate(context: ExtensionContext) {
+
+	console.log('Perl Navigator web client activated!');
+
+	/* 
+	 * all except the code to create the language client in not browser specific
+	 * and could be shared with a regular (Node) extension
+	 */
+	const documentSelector = [
+		{ scheme: 'file', language: 'perl' },
+		{ scheme: 'untitled', language: 'perl' }
+	]
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		documentSelector,
+		synchronize: {},
+		initializationOptions: {}
+	};
+
+	const client = createWorkerLanguageClient(context, clientOptions);
+
+	const disposable = client.start();
+	context.subscriptions.push(disposable);
+
+	client.onReady().then(() => {
+		console.log('Perl Navigator web client is now ready!');
+	});
+}
+
+function createWorkerLanguageClient(context: ExtensionContext, clientOptions: LanguageClientOptions) {
+	// Create a worker. The worker main file implements the language server.
+	const serverMain = Uri.joinPath(context.extensionUri, 'browser-ext/dist/browserServerMain.js');
+	const worker = new Worker(serverMain.toString(true));
+
+	// create the language server client to communicate with the server running in the worker
+	return new LanguageClient('perlnavigator-web', 'Perl Navigator Web', clientOptions, worker);
+}
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//@ts-check
+'use strict';
+
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+const path = require('path');
+
+/** @type WebpackConfig */
+const browserClientConfig = {
+	context: path.join(__dirname, 'client'),
+	mode: 'none',
+	target: 'webworker', // web extensions run in a webworker context
+	entry: {
+		browserClientMain: './src/browserClientMain.ts',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, 'client', 'dist'),
+		libraryTarget: 'commonjs',
+	},
+	resolve: {
+		mainFields: ['module', 'main'],
+		extensions: ['.ts', '.js'], // support ts-files and js-files
+		alias: {},
+		fallback: {
+			path: require.resolve('path-browserify'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+		],
+	},
+	externals: {
+		vscode: 'commonjs vscode', // ignored because it doesn't exist
+	},
+	performance: {
+		hints: false,
+	},
+	devtool: 'source-map',
+};
+
+/** @type WebpackConfig */
+const browserServerConfig = {
+	context: path.join(__dirname, 'browser-ext'),
+	mode: 'none',
+	target: 'webworker', // web extensions run in a webworker context
+	entry: {
+		browserServerMain: './src/browserServerMain.ts',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, 'browser-ext', 'dist'),
+		libraryTarget: 'var',
+		library: 'serverExportVar',
+	},
+	resolve: {
+		mainFields: ['module', 'main'],
+		extensions: ['.ts', '.js'], // support ts-files and js-files
+		alias: {},
+		fallback: {
+			//path: require.resolve("path-browserify")
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+		],
+	},
+	externals: {
+		vscode: 'commonjs vscode', // ignored because it doesn't exist
+	},
+	performance: {
+		hints: false,
+	},
+	devtool: 'source-map',
+};
+
+module.exports = [browserClientConfig, browserServerConfig];

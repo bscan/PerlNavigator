988f78a1ac83108dc6f1523f44ec24f432092426
James Tirta Halim <tirtajames45@gmail.com>
2023-10-15 12:39:28 +0700

    refactor: change chain of logical operators to nested ifs

--- server/src/completion.ts
+++ server/src/completion.ts
@@ -117,41 +117,48 @@ function getMatches(perlDoc: PerlDocument, symbol: string,  replace: Range): Com
     // const lcQualifiedSymbol = qualifiedSymbol.toLowerCase(); Case insensitive matches are hard since we restore what you originally matched on
 
     perlDoc.elems.forEach((elements: PerlElem[], elemName: string) => {
-        if(/^[\$\@\%].$/.test(elemName)) return; // Remove single character magic perl variables. Mostly clutter the list
+        if (/^[\$\@\%].$/.test(elemName))
+	    return; // Remove single character magic perl variables. Mostly clutter the list
 
         let element = perlDoc.canonicalElems.get(elemName) || elements[0]; // Get the canonical (typed) element, otherwise just grab the first one.
 
         // All plain and inherited subroutines should match with $self. We're excluding PerlSymbolKind.ImportedSub here because imports clutter the list, despite perl allowing them called on $self->
-        if(bSelf && [PerlSymbolKind.LocalSub,
+        if (bSelf && [PerlSymbolKind.LocalSub,
 		    PerlSymbolKind.Inherited,
 		    PerlSymbolKind.LocalMethod,
-		    PerlSymbolKind.Field].includes(element.type) ) elemName = `$self::${elemName}`;
+		    PerlSymbolKind.Field].includes(element.type))
+	    elemName = `$self::${elemName}`;
 
-        if (goodMatch(perlDoc, elemName, qualifiedSymbol, symbol, bKnownObj)){
+        if (goodMatch(perlDoc, elemName, qualifiedSymbol, symbol, bKnownObj)) {
             // Hooray, it's a match! 
             // You may have asked for FOO::BAR->BAZ or $qux->BAZ and I found FOO::BAR::BAZ. Let's put back the arrow or variable before sending
             const quotedSymbol = qualifiedSymbol.replace(/([\$])/g, '\\$1'); // quotemeta for $self->FOO
             let aligned = elemName.replace(new RegExp(`^${quotedSymbol}`, 'gi'), symbol);
 
-            if(symbol.endsWith('-')) aligned = aligned.replace(new RegExp(`-:`, 'gi'), '->');  // Half-arrows count too
+            if (symbol.endsWith('-'))
+	       aligned = aligned.replace(new RegExp(`-:`, 'gi'), '->');  // Half-arrows count too
 
             // Don't send invalid constructs
-            if(/\-\>\w+::/.test(aligned) || // like FOO->BAR::BAZ
-	       (/\-\>\w+$/.test(aligned) && ![PerlSymbolKind.LocalSub,
-                                            PerlSymbolKind.ImportedSub,
-                                            PerlSymbolKind.Inherited,
-                                            PerlSymbolKind.LocalMethod,
-                                            PerlSymbolKind.Method,
-                                            PerlSymbolKind.Field,
-                                            PerlSymbolKind.PathedField].includes(element.type)) || // FOO->BAR if Bar is not a sub/method.
-                   (!/^\$.*\-\>\w+$/.test(aligned) && [PerlSymbolKind.LocalMethod,
-                                                      PerlSymbolKind.Method,
-						      PerlSymbolKind.Field,
-						      PerlSymbolKind.PathedField].includes(element.type)) || // FOO::BAR if Bar is a instance method or attribute (I assume them to be instance methods/attributes, not class)
-                   (/-:/.test(aligned)) ||  // We look things up like this, but don't let them slip through
-                   (/^\$.*::/.test(aligned)) // $Foo::Bar, I don't really hunt for these anyway             
-              ) return;
-
+	    // like FOO->BAR::BAZ
+	    if (/\-\>\w+::/.test(aligned)
+	       || /\-\>\w+$/.test(aligned))
+		 // FOO->BAR if Bar is not a sub/method.
+		 if (![PerlSymbolKind.LocalSub,
+                    PerlSymbolKind.ImportedSub,
+                    PerlSymbolKind.Inherited,
+                    PerlSymbolKind.LocalMethod,
+                    PerlSymbolKind.Method,
+                    PerlSymbolKind.Field,
+                    PerlSymbolKind.PathedField].includes(element.type)
+		    || !/^\$.*\-\>\w+$/.test(aligned))
+		    // FOO::BAR if Bar is a instance method or attribute (I assume them to be instance methods/attributes, not class)
+		     if ([PerlSymbolKind.LocalMethod,
+                        PerlSymbolKind.Method,
+			PerlSymbolKind.Field,
+			PerlSymbolKind.PathedField].includes(element.type)
+			|| /-:/.test(aligned) // We look things up like this, but don't let them slip through
+			|| /^\$.*::/.test(aligned)) // $Foo::Bar, I don't really hunt for these anyway
+		         return;
             matches = matches.concat(buildMatches(aligned, element, replace));
         }
     });
@@ -162,26 +169,25 @@ function getMatches(perlDoc: PerlDocument, symbol: string,  replace: Range): Com
 
 // TODO: preprocess all "allowed" matches so we don't waste time iterating over them for every autocomplete.
 function goodMatch(perlDoc: PerlDocument, elemName: string, qualifiedSymbol: string, origSymbol: string, bKnownObj: boolean): boolean {
-
-    if(!elemName.startsWith(qualifiedSymbol)) return false;
-
+    if (!elemName.startsWith(qualifiedSymbol))
+        return false;
     // All uppercase methods are generally private or autogenerated and unhelpful
-    if(/(?:::|->)[A-Z][A-Z_]+$/.test(elemName)) return false;
-
+    if(/(?:::|->)[A-Z][A-Z_]+$/.test(elemName))
+        return false;
     if(bKnownObj){
         // If this is a known object type, we probably aren't importing the package or building a new one.
-        if(/(?:::|->)(?:new|import)$/.test(elemName)) return false;
-
+        if (/(?:::|->)(?:new|import)$/.test(elemName))
+	    return false;
         // If we known the object type (and variable name is not $self), then exclude the double underscore private variables (rare anyway. single underscore kept, but ranked last in the autocomplete)
-        if((/^(?!\$self)\$/.test(origSymbol) && /(?:::|->)__\w+$/.test(elemName))) return false;
-
+        if ((/^(?!\$self)\$/.test(origSymbol) && /(?:::|->)__\w+$/.test(elemName)))
+	    return false;
         // Otherwise, always autocomplete, even if the module has not been explicitly imported.
         return true;
     }
     // Get the module name to see if it's been imported. Otherwise, don't allow it.
     let modRg = /^(.+)::.*?$/;
     var match = modRg.exec(elemName);
-    if(match && !perlDoc.imported.has(match[1])){
+    if (match && !perlDoc.imported.has(match[1])) {
         // TODO: Allow completion on packages/class defined within the file itself (e.g. Foo->new, $foo->new already works)
         // Thing looks like a module, but was not explicitly imported
         return false;
@@ -265,9 +271,8 @@ function buildMatches(lookupName: string, elem: PerlElem, range: Range): Complet
             break;
         }
     }
-    if(docs.length>0){
+    if (docs.length > 0)
         documentation = {kind: "markdown", value: "```\n" + docs.join("\n") + "\n```" };
-    }
     
     let labelsToBuild = [lookupName];
 

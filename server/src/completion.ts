ee190fe9cee0a9ed0d29f0ead67c2b9826fbc0c3
James Tirta Halim <tirtajames45@gmail.com>
2023-10-16 12:56:43 +0700

    lCh is unused?

--- server/src/completion.ts
+++ server/src/completion.ts
@@ -45,12 +45,11 @@ function getPrefix(text: string, position: number): CompletionPrefix {
     let l = position - 1; // left
     for (; l >= 0 && canShift(text[l]); --l);
     let lCh = "";
-    if (l >= 0 && (text[l] == "$" || text[l] == "@" || text[l] == "%")) {
+    if (l >= 0 && (text[l] == "$" || text[l] == "@" || text[l] == "%"))
         lCh = text[l];
-    } else {
+    else
         ++l;
-    }
-    const symbol = text.substring(l, position);
+    const symbol = lCh + text.substring(l, position);
     return { symbol: symbol, charStart: l, charEnd: position };
 }
 
--- server/src/signatures.ts
+++ server/src/signatures.ts
@@ -41,12 +41,11 @@ function getFunction(position: Position, txtDoc: TextDocument): string[] {
         // Allow for ->, but not => or > (e.g. $foo->bar, but not $foo=>bar or $foo>bar).
         if (text[l] == ">") if (l - 1 >= 0 && text[l - 1] != "-") break;
     let lCh = "";
-    if (l >= 0 && (text[l] == "$" || text[l] == "@" || text[l] == "%")) {
+    if (l >= 0 && (text[l] == "$" || text[l] == "@" || text[l] == "%"))
         lCh = text[l];
-    } else {
+    else
         ++l;
-    }
-    const symbol = text.substring(l, r);
+    const symbol = lCh + text.substring(l, r);
     const currSig = text.substring(r, index);
     return [symbol, currSig];
 }
